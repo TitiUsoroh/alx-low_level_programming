7b9ca73617e0c273217aa4a62723220fa7bd62d4
TitiUsoroh <etienousoroh777@gmail.com>
2022-09-08 16:06:18 -0700

    My hello commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the string in the printf function
+ *
+ *  Description: this program prints
+ *
+ *  'with proper grammar, but the outcome is a piece of art'
+ *
+ *  using the printf function
+ *
+ *  Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
