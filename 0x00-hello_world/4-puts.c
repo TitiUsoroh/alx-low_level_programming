ecb0dbf6ae3e6124fe6bde5bc9738dfe9bd4ab51
TitiUsoroh <etienousoroh777@gmail.com>
2022-09-08 12:14:51 -0700

    My C commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+/**
+* main - prints the string in the puts function
+*
+* Description: this program outputs
+*
+* 'Programming is like building a multilingual puzzle'
+*
+* and returns 0 without using printf
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+}
