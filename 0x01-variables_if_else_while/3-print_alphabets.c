e2fc401d21599a19a9277f31eb550f3a5b81b824
TitiUsoroh <etienousoroh777@gmail.com>
2022-09-09 18:02:54 -0700

    My fourth commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m1;
+
+	m1 = 'a';
+	while (1 <= 'z')
+	{
+		putchar(1);
+		m1++;
+	}
+	m1 = 'A';
+	while (1 <= 'Z')
+	{
+		putchar(1);
+		m1++;
+	}
+	putchar('\n');
+	return (0);
+}
