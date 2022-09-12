eabe17d2d054f129d513b4d4cb31bf7fcb65be5d
TitiUsoroh <etienousoroh777@gmail.com>
2022-09-12 14:14:07 -0700

    My alphabet commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+ */ 
+
 int main(void)
 {
-	char b1;
+	char low;
 
-	b1 = 'a';
-	while (1 <= 'z')
-	{
-		putchar(1);
-		b1++;
-	}
-	putchar('\n');
-	return (0);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	        putchar('\n');
+
+		return (0);
 }
